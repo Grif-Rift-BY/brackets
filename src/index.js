002522a1c46c8eba4848d0765225141a1ee914c2
Grif-Rift-BY <pavel.zalazaev@protonmail.com>
2022-07-15 03:34:44 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check( str, bracketsConfig ) {
+  let length = str.length;
+  let templates = bracketsConfig.map( ( item ) => {
+    if ( isNaN( +item[ 0 ] ) ) {
+      return "\\" + item[ 0 ] + "\\" + item[ 1 ];
+    } else {
+      return item[ 0 ] + item[ 1 ];
+    };
+  } ).join( '|' );
+  regex = new RegExp( `${templates}` );
+  for ( let i = 0; i < length; i++ ) str = str.replace( regex, '' );
+  return str.length > 0 ? false : true ;
 }
